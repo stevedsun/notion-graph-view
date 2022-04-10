7bb42929011573de8e136f75fa0929db61fb8f86
Steve Sun <>
2022-04-10 16:08:12 +0800

    UPDATE: config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from notion_client import Client
 
 
-NOTION_TOKEN = "secret_EaIWwCzo3SXEC6SJp2Gl2AETbuZHUUhAICTyHpoP2ch"
-PAGE_ID = "75a99b1646474dd981ed487b59f99e06"
+NOTION_TOKEN = "<your-sercet-token-here>"
+PAGE_ID = "<base-page-id-here>"
 
 notion = Client(auth=NOTION_TOKEN)
