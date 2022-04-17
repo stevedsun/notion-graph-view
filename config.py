8d2bafae98b1c3e616989f3f0880b2fa263976f0
Steve Sun <>
2022-04-17 15:40:46 +0800

    update

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
+import os
 from notion_client import Client
 
 
-NOTION_TOKEN = "<your-sercet-token-here>"
-PAGE_ID = "<base-page-id-here>"
+# Paste your NOTION token (e.g. secret_balabalabalabala) and page id (e.g. 43a57ade6650460b980e96fde16b03f1) in "" below.
+NOTION_TOKEN = ""
+PAGE_ID = ""
+
+if not NOTION_TOKEN:
+    NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+
+if not PAGE_ID:
+    PAGE_ID = os.getenv("PAGE_ID")
 
 notion = Client(auth=NOTION_TOKEN)
