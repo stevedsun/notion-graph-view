41ea55dd8eb30432ee9c94b6be019bb4bdceb365
Di Sun <di.sun@siemens.com>
2025-03-18 11:33:55 +0800

    update

--- notion_graph/parser.py
+++ notion_graph/parser.py
@@ -12,10 +12,13 @@ column_list -> column -> block
 table -> table_row -> block
 '''
 
-from typing import Any
-import time
-from notion_client import Client, APIResponseError, APIErrorCode
+import asyncio
+import logging
+from typing import Any, Callable
+
+from notion_client import APIErrorCode, APIResponseError, Client
 from pyvis.network import Network
+
 from .helper import contains_mention_or_relation_type, is_same_block_id
 
 __all__ = ["Parser"]
@@ -35,6 +38,8 @@ SUPPORTED_PAGE_PROPERTY_TYPES = [
 COLOR_BG = "#fff"
 COLOR_NODE = "#757575"
 
+logging.basicConfig(level=logging.INFO)
+
 
 class Parser:
     def __init__(self, notion_version: str, bearer_token: str) -> None:
@@ -44,173 +49,110 @@ class Parser:
             font_color=True,
             height="750px",
             cdn_resources="in_line")
+        self._loop = asyncio.get_event_loop()
 
-    def parse(self, root_id: str) -> Network:
-        print('Parsing ...')
-        self._parse_block(root_id)
-        print('Prasing ... done')
+    async def parse(self, root_id: str) -> Network:
+        logging.info('Parsing ...')
+        await self._parse_block(root_id)
+        logging.info('Parsing ... done')
         return self._graph
 
-    def export_to_html(self, file_path: str):
-        print('Graph is generated at:', file_path)
+    async def export_to_html(self, file_path: str) -> None:
+        logging.info('Graph is generated at: %s', file_path)
         self._graph.repulsion(node_distance=200, spring_length=200)
         html = self._graph.generate_html()
         with open(file_path, mode='w', encoding='utf-8') as fp:
             fp.write(html)
 
-    def _parse_block(self, root_id: str, obj: Any = None) -> None:
+    async def _parse_block(self, root_id: str, obj: Any = None) -> None:
         if obj is None:
-            try:
-                obj = self._notion.blocks.retrieve(root_id)
-            except APIResponseError:
-                if APIResponseError.code == APIErrorCode.RateLimited:
-                    time.sleep(1)
-                    obj = self._notion.blocks.retrieve(root_id)
-                else:
-                    return
+            obj = await self._retrieve_with_retry(
+                self._notion.blocks.retrieve, root_id)
+            if obj is None:
+                return
 
-        self._parse_block_object(obj, root_id)
+        await self._parse_block_object(obj, root_id)
 
-    def _parse_database(self, id: str, db: Any = None, parent_page_or_database_id: str = "") -> None:
+    async def _parse_database(self, id: str, db: Any = None, parent_page_or_database_id: str = "") -> None:
         if db is None:
-            try:
-                db = self._notion.databases.retrieve(id)
-            except APIResponseError:
-                if APIResponseError.code == APIErrorCode.RateLimited:
-                    time.sleep(1)
-                    db = self._notion.databases.retrieve(id)
-                else:
-                    return
+            db = await self._retrieve_with_retry(self._notion.databases.retrieve, id)
+            if db is None:
+                return
 
         if db['archived']:
             return
 
         self._add_node(db)
         self._add_edge(parent_page_or_database_id, id)
-        self._parse_database_pages(id)
+        await self._parse_database_pages(id)
 
-    def _parse_page(self, id: str, page: Any = None, parent_page_or_database_id: str = "") -> None:
+    async def _parse_page(self, id: str, page: Any = None, parent_page_or_database_id: str = "") -> None:
         if page is None:
-            try:
-                page = self._notion.pages.retrieve(id)
-            except APIResponseError:
-                if APIResponseError.code == APIErrorCode.RateLimited:
-                    time.sleep(1)
-                    page = self._notion.pages.retrieve(id)
-                else:
-                    return
+            page = await self._retrieve_with_retry(self._notion.pages.retrieve, id)
+            if page is None:
+                return
 
         if page['archived']:
             return
 
         self._add_node(page)
         self._add_edge(parent_page_or_database_id, id)
-        self._parse_page_properties(page['properties'], id)
-        self._parse_block_children(id, id)
+        await self._parse_page_properties(page['properties'], id)
+        await self._parse_block_children(id, id)
 
-    def _parse_block_object(self, obj: dict, parent_page_or_database_id: str = "") -> None:
-        '''API Ref: https://developers.notion.com/reference/block#block-type-object
-
-        Example:
-        {
-            "object": "block",
-            "id": "136a23da-e2cf-4438-b40b-f9fb779b3172",
-            "parent": {
-                "type": "page_id",
-                "page_id": "8080c51b-6c93-4342-9f4f-7d9f358ab9ba"
-            },
-            "created_time": "2023-01-19T23:54:00.000Z",
-            "last_edited_time": "2023-01-19T23:54:00.000Z",
-            "created_by": {
-                "object": "user",
-                "id": "1237204c-1cfe-4ddb-a8bd-61865a28f5d3"
-            },
-            "last_edited_by": {
-                "object": "user",
-                "id": "1237204c-1cfe-4ddb-a8bd-61865a28f5d3"
-            },
-            "has_children": false,
-            "archived": false,
-            "type": "paragraph",
-            "paragraph": {
-                "rich_text": [
-                    {
-                        "type": "mention",
-                        "mention": {
-                            "type": "page",
-                            "page": {
-                                "id": "62788faf-1f04-4b33-9e10-f94a7ec1ddbf"
-                            }
-                        },
-                        "annotations": {
-                            "bold": false,
-                            "italic": false,
-                            "strikethrough": false,
-                            "underline": false,
-                            "code": false,
-                            "color": "default"
-                        },
-                        "plain_text": "database_page_1_sub-page",
-                        "href": "https://www.notion.so/62788faf1f044b339e10f94a7ec1ddbf"
-                    },
-                    {
-                        "type": "text",
-                        "text": {
-                            "content": " ",
-                            "link": null
-                        },
-                        "annotations": {
-                            "bold": false,
-                            "italic": false,
-                            "strikethrough": false,
-                            "underline": false,
-                            "code": false,
-                            "color": "default"
-                        },
-                        "plain_text": " ",
-                        "href": null
-                    }
-                ],
-                "color": "default"
-            }
-        }
-        '''
+    async def _parse_block_object(self, obj: dict, parent_page_or_database_id: str = "") -> None:
         if obj['type'] not in SUPPORTED_BLOCK_TYPES or obj['archived']:
             return
 
         if obj['type'] == 'child_database':
-            self._parse_database(obj['id'], None, parent_page_or_database_id)
+            await self._parse_database(obj['id'], None, parent_page_or_database_id)
             return
 
         if obj['type'] == 'child_page':
-            self._parse_page(obj['id'], None, parent_page_or_database_id)
+            await self._parse_page(obj['id'], None, parent_page_or_database_id)
             return
 
-        # column_list -> column -> block
         if obj['type'] == 'column_list' or obj['type'] == 'column':
             if obj['has_children']:
-                self._parse_block_children(
+                await self._parse_block_children(
                     obj['id'], parent_page_or_database_id)
             return
 
-        obj_value = obj[obj['type']]
-        # For the supported types with rich_text
-        rich_text_list = obj_value.get(
-            'rich_text', None)
-        if rich_text_list:
-            self._parse_rich_text_list(
-                rich_text_list, parent_page_or_database_id)
+        await self._parse_block_content(obj, parent_page_or_database_id)
 
-        # table -> table_row -> cells
-        cells_metrics = obj_value.get('cells', None)
-        if cells_metrics:
-            self._parse_cells_metrics(
-                cells_metrics, parent_page_or_database_id)
+    async def _parse_block_children(self, block_id: str, parent_page_or_database_id: str) -> None:
+        list_object = None
+        try:
+            list_object = await self._retrieve_with_retry(
+                self._notion.blocks.children.list, block_id)
+        except Exception as e:
+            logging.error("Error retrieving block children: %s", e)
+        if list_object is None:
+            return
 
-        if obj.get('is_toggleable', False) or obj.get('has_children', False):
-            self._parse_block_children(obj['id'], parent_page_or_database_id)
+        if isinstance(list_object, dict):
+            block_list = list_object['results']
 
-    def _parse_page_properties(self, prop_obj: dict, parent_page_or_database_id: str) -> None:
+            for block in block_list:
+                await self._parse_block_object(block, parent_page_or_database_id)
+
+    async def _parse_database_pages(self, database_id: str) -> None:
+        has_more = True
+        next_cursor = None
+        while has_more:
+            data = await self._retrieve_with_retry(
+                self._notion.databases.query, database_id, page_size=100, start_cursor=next_cursor)
+            if data is None:
+                return
+
+            if isinstance(data, dict):
+                pages = data['results']
+                has_more = data['has_more']
+                next_cursor = data['next_cursor']
+                for page in pages:
+                    await self._parse_page(page['id'], None, database_id)
+
+    async def _parse_page_properties(self, prop_obj: dict, parent_page_or_database_id: str) -> None:
         '''Search page properties which contains "mention" or "relation" type.
 
         No need to deep search into relation pages, because if the relation page is under root page,
@@ -225,55 +167,32 @@ class Parser:
                 return
 
             if i['type'] == 'relation':
-                self._retrieve_relation_page_title(
+                await self._retrieve_relation_page_title(
                     i['relation'], parent_page_or_database_id)
             if i['type'] == 'rich_text' or i['type'] == 'title':
-                self._parse_rich_text_list(
+                await self._parse_rich_text_list(
                     i[i['type']], parent_page_or_database_id)
 
-    def _parse_database_pages(self, database_id: str) -> None:
-        has_more = True
-        next_cursor = None
-        while has_more:
-            try:
-                data = self._notion.databases.query(
-                    database_id, page_size=100, start_cursor=next_cursor)
-            except APIResponseError:
-                if APIResponseError.code == APIErrorCode.RateLimited:
-                    time.sleep(1)
-                    data = self._notion.databases.query(
-                        database_id, page_size=100, start_cursor=next_cursor)
-                else:
-                    return
+    async def _parse_block_content(self, obj: dict, parent_page_or_database_id: str) -> None:
+        obj_value = obj[obj['type']]
+        rich_text_list = obj_value.get('rich_text', None)
+        if rich_text_list:
+            await self._parse_rich_text_list(
+                rich_text_list, parent_page_or_database_id)
 
-            if isinstance(data, dict):
-                pages = data['results']
-                has_more = data['has_more']
-                next_cursor = data['next_cursor']
-                for page in pages:
-                    self._parse_page(page['id'], None, database_id)
+        cells_metrics = obj_value.get('cells', None)
+        if cells_metrics:
+            await self._parse_cells_metrics(
+                cells_metrics, parent_page_or_database_id)
 
-    def _parse_block_children(self, block_id: str, parent_page_or_database_id: str) -> None:
-        try:
-            list_object = self._notion.blocks.children.list(block_id)
-        except APIResponseError:
-            if APIResponseError.code == APIErrorCode.RateLimited:
-                time.sleep(1)
-                list_object = self._notion.blocks.children.list(block_id)
-            else:
-                return
+        if obj.get('is_toggleable', False) or obj.get('has_children', False):
+            await self._parse_block_children(obj['id'], parent_page_or_database_id)
 
-        if isinstance(list_object, dict):
-            block_list = list_object['results']
-
-            for block in block_list:
-                self._parse_block_object(block, parent_page_or_database_id)
-
-    def _parse_cells_metrics(self, cells_metrics: list, parent_page_or_database_id: str) -> None:
+    async def _parse_cells_metrics(self, cells_metrics: list, parent_page_or_database_id: str) -> None:
         for row_cells in cells_metrics:
-            self._parse_rich_text_list(row_cells, parent_page_or_database_id)
+            await self._parse_rich_text_list(row_cells, parent_page_or_database_id)
 
-    def _parse_rich_text_list(self, rich_text_list: list, parent_page_or_database_id: str) -> None:
+    async def _parse_rich_text_list(self, rich_text_list: list, parent_page_or_database_id: str) -> None:
         '''Example:
 
         [
@@ -303,10 +222,10 @@ class Parser:
 
         for i in rich_text_list:
             if i['type'] == 'mention':
-                self._retrieve_mention_object_title(
+                await self._retrieve_mention_object_title(
                     i['mention'], parent_page_or_database_id)
 
-    def _retrieve_relation_page_title(self, relation_list: list, parent_page_or_database_id: str, **kwargs):
+    async def _retrieve_relation_page_title(self, relation_list: list, parent_page_or_database_id: str, **kwargs):
         '''Example:
 
         [
@@ -316,20 +235,16 @@ class Parser:
         ]
         '''
         for relation_obj in relation_list:
-            try:
-                page = self._notion.pages.retrieve(relation_obj['id'])
-            except APIResponseError:
-                if APIResponseError.code == APIErrorCode.RateLimited:
-                    time.sleep(1)
-                    page = self._notion.pages.retrieve(relation_obj['id'])
-                else:
-                    continue
+            page = await self._retrieve_with_retry(
+                self._notion.pages.retrieve, relation_obj['id'])
+            if page is None:
+                continue
 
             if isinstance(page, dict):
                 self._add_node(page)
                 self._add_edge(parent_page_or_database_id, page['id'])
 
-    def _retrieve_mention_object_title(self, mention_obj: dict, parent_page_or_database_id: str):
+    async def _retrieve_mention_object_title(self, mention_obj: dict, parent_page_or_database_id: str):
         '''Example:
 
         {
@@ -340,16 +255,10 @@ class Parser:
         }
         '''
         if mention_obj['type'] == 'page':
-            try:
-                page = self._notion.pages.retrieve(
-                    mention_obj[mention_obj['type']]['id'])
-            except APIResponseError:
-                if APIResponseError.code == APIErrorCode.RateLimited:
-                    time.sleep(1)
-                    page = self._notion.pages.retrieve(
-                        mention_obj[mention_obj['type']]['id'])
-                else:
-                    return
+            page = await self._retrieve_with_retry(
+                self._notion.pages.retrieve, mention_obj[mention_obj['type']]['id'])
+            if page is None:
+                return
 
             if isinstance(page, dict):
                 self._add_node(page)
@@ -378,7 +287,7 @@ class Parser:
             else:
                 title = block[block['type']]['title']
 
-        print("+node:", title)
+        logging.info("+node: %s", title)
         self._graph.add_node(
             block['id'],
             label=title,
@@ -392,3 +301,13 @@ class Parser:
             return
 
         self._graph.add_edge(lnode_id, rnode_id)
+
+    async def _retrieve_with_retry(self, func: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
+        try:
+            return await func(*args, **kwargs)
+        except APIResponseError as e:
+            if e.code == APIErrorCode.RateLimited:
+                await asyncio.sleep(1)
+                return await func(*args, **kwargs)
+            else:
+                return None
